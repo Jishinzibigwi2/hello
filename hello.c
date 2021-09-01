4160625d70e417cd4ee0b6db90eddc54676bae6e
Jean de Dieu Ishinzibigwi <jishinzibigwi2@huskers.unl.edu>
2021-09-01 02:17:59 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Jean de Dieu Ishinzibigwi
+ *         cbourke@cse.unl.edu
+ * Date: 2020/08/31
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Jean de Dieu Ishinzibigwi!\n");
+
+  return 0;
+}
